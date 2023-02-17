6f93ff9bafa109abb7b471933226b84d7fce4bc1
kamalkech <maroc.develop@gmail.com>
2023-02-17 15:24:10 +0100

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! / nestjs + github-action + aws updated';
+    return 'Hello World! / nestjs + github-action + aws success';
   }
 }
